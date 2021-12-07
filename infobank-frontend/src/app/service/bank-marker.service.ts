516bcc72f36b3ca87a13c155a0030eb3cc66a641
lukaj <luka.jovanovski@hotmail.com>
2021-12-07 18:12:13 +0100

    Operators Http fetch from backend

--- infobank-frontend/src/app/service/bank-marker.service.ts
+++ infobank-frontend/src/app/service/bank-marker.service.ts
@@ -1,7 +1,8 @@
-import {Injectable, OnInit} from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import * as L from 'leaflet';
 import { PopupService } from './popup.service';
+import {catchError, Observable, throwError} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -35,13 +36,24 @@ export class BankMarkerService {
     });
     }
 
-    getOperators() : any
+    getOperators() : Observable<any>
     {
-      let data:any = [''];
-      this.http.get(this.usersUrl+"/operators").subscribe(res => {
-        console.log(res);
-        data = res;
-      });
-      return data;
+      return this.http.get(this.usersUrl+"/operators")    ;
+      //return this.http.jsonp(this.usersUrl+"/operators",'callback').pipe(catchError(this.handleError));
+    }
+
+    private handleError(error: HttpErrorResponse) {
+      if (error.status === 0) {
+        // A client-side or network error occurred. Handle it accordingly.
+        console.error('An error occurred:', error.error);
+      } else {
+        // The backend returned an unsuccessful response code.
+        // The response body may contain clues as to what went wrong.
+        console.error(
+          `Backend returned code ${error.status}, body was: `, error.error);
+      }
+      // Return an observable with a user-facing error message.
+      return throwError(
+        'Something bad happened; please try again later.');
     }
 }
