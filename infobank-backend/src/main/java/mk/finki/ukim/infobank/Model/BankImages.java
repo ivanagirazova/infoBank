9af9ee3668f85af60197042c60bfe3cdd6bee5ea
ivanagirazova <ivanagirazova@gmail.com>
2022-01-30 19:37:26 +0100

    bank images non-used imports removed

--- infobank-backend/src/main/java/mk/finki/ukim/infobank/Model/BankImages.java
+++ infobank-backend/src/main/java/mk/finki/ukim/infobank/Model/BankImages.java
@@ -4,15 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
-import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Document
 public class BankImages {
     @Id
     private String id;
